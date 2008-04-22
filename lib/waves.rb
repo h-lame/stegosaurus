262f9f8f3f1f10ee3dd63aa763528c32da48cac9
Murray Steele <muz@h-lame.com>
2008-04-22 22:30:11 +0100

    Now we can make waves!
    But should we?  I mean, waves are pretty scary if you don't have sealegs.
    
    *Being Serious*
    It's a port from the python, so not very rubyish.  Could probably do with much more poking at.  And a nicer interface.  And tests.  Oh god yes, the tests.

--- lib/waves.rb
+++ lib/waves.rb
@@ -1,110 +1,128 @@
-# from struct import *
-# import os, sys
+# RIFF hdr + FMT chunk + DATA chunk
+# -- RIFF hdr -- 
+# CHUNKID       4bytes = "RIFF"
+# CHUNKSIZE     4bytes = 36+data size
+# FORMAT        4bytes = "WAVE"
+# -- FMT chunk --
+# SUBCHUNK1ID   4bytes = "fmt "
+# SUBCHUNK1Size 4bytes = 16 (PCM)
+# AUDIOFORMAT   2bytes = 1 (PCM)
+# NUMCHANNELS   2bytes = 1 (mono), 2 (stereo)
+# SAMPLERATE    4bytes = some sample rate
+# BYTERATE      4bytes = SAMPLERATE * BLOCKALIGN
+# BLOCKALIGN    2bytes = NUMCHANNELS * BITSPERSAMPLE / 8
+# BITSPERSAMPLE 2bytes = 8, 16, 32 etc..
+# -- DATA chunk --
+# SUBCHUNK2ID   4bytes = "data"
+# SUBCHUNK2SIZE 4bytes = NUMSAMPLES * NUMCHANNELS * BITSPERSAMPLE / 8
+# DATA          ....
+
+# TODO: some kinda callback registration so we can know what's happening
+# during the various steps
 # 
-# # RIFF hdr + FMT chunk + DATA chunk
-# 
-# # -- RIFF hdr -- 
-# # CHUNKID       4bytes = "RIFF"
-# # CHUNKSIZE     4bytes = 36+data size
-# # FORMAT        4bytes = "WAVE"
-# 
-# # -- FMT chunk --
-# # SUBCHUNK1ID   4bytes = "fmt "
-# # SUBCHUNK1Size 4bytes = 16 (PCM)
-# # AUDIOFORMAT   2bytes = 1 (PCM)
-# # NUMCHANNELS   2bytes = 1 (mono), 2 (stereo)
-# # SAMPLERATE    4bytes = some sample rate
-# # BYTERATE      4bytes = SAMPLERATE * BLOCKALIGN
-# # BLOCKALIGN    2bytes = NUMCHANNELS * BITSPERSAMPLE / 8
-# # BITSPERSAMPLE 2bytes = 8, 16, 32 etc..
-# 
-# # -- DATA chunk --
-# # SUBCHUNK2ID   4bytes = "data"
-# # SUBCHUNK2SIZE 4bytes = NUMSAMPLES * NUMCHANNELS * BITSPERSAMPLE / 8
-# # DATA          ....
-# 
-# # TODO: some kinda callback registration so we can know what's happening
-# # during the various steps
-# 
-# class WAVMaker:
-#     def __init__(self, channels = None, samplerate = None, bps = None):
-#         self.channels = 1
-#         self.samplerate = 22050
-#         self.bps = 8
-#         self.bufsize = 128
-#         if channels:
-#             self.channels = int(channels)
-#         if samplerate:
-#             self.samplerate = int(samplerate)
-#         if bps:
-#             self.bps = int(bps)
-#     
-#     def getNumSamplesFromData(self, filename):
-#         if os.path.exists(filename):
-#             fsize = os.path.getsize(filename)
-#             numsamples = (((fsize * 8) / self.channels + 0.0) / self.bps + 0.0)
-#             # I dunno, maybe I'll need to pad the file if this is a floaty value?
-#             print numsamples
-#             return numsamples
-# 
-#     def makeWAVHeader(self, filename):
-#         if os.path.exists(filename):
-#             fsize = os.path.getsize(filename)
-#             riff = pack(">4c","R","I","F","F")
-#             riff += pack("<i",36+fsize)
-#             riff += pack(">4c","W","A","V","E")
-#             
-#             fmt = pack(">4c","f","m","t"," ")
-#             
-#             self.getNumSamplesFromData(filename)
-#             
-#             blockalign = self.channels * self.bps / 8
-#             byterate = self.samplerate * blockalign
-#             
-#             fmt += pack("<i2h2i2h",16,1,self.channels,self.samplerate,byterate,blockalign,self.bps)
-#         
-#             data = pack(">4c","d","a","t","a")
-#             data += pack("<i",fsize)
-#             
-#             return (riff, fmt, data)
-#             
-#     def getWAVFilename(self, filename):
-#         wav_filename = "%s.wav" % filename
-#         i = 0
-#         while os.path.exists(wav_filename):
-#             wav_filename = "%s%03d.wav" % (filename, i)
-#             i+=1
-#             if i > 999:
-#                 raise Exception, "Too many wav files alread for this file"
-#         return wav_filename
-#     
-#     def writeWAVFile(self, filename, header, datafilename):
-#         (riff, fmt, datahdr) = header
-#         wav_file = file(filename, "w+b")
-#         wav_file.write(riff)
-#         wav_file.write(fmt)
-#         wav_file.write(datahdr)
-#         wav_file.flush()
-#             
-#         data_file = file(datafilename,"rb")
-#         data = data_file.read(self.bufsize)
-#         while data != "":
-#             wav_file.write(data)
-#             data = data_file.read(self.bufsize)
-#         wav_file.flush()
-#         wav_file.close()
-#         data_file.close()
-# 
-#     def makeWAVFile(self, filename):
-#         filename = os.path.abspath(filename)
-#         if os.path.exists(filename):
-#             wav_header = self.makeWAVHeader(filename)
-#             wav_filename = self.getWAVFilename(filename)
-#             self.writeWAVFile(wav_filename, wav_header, filename)
-#     
-# def main(fname, channels = None, samplerate = None, bps = None):
-#     wm = WAVMaker(channels, samplerate, bps)
-#     wm.makeWAVFile(fname)
-# 
-# if __name__ == '__main__':
-#     main(*sys.argv[1:])
+
+class Waves
+  attr_accessor :channels, :sample_rate, :bps
+  
+  def self.mono()
+    new(:mono, 22050, 8)
+  end
+  
+  def self.stereo()
+    new(:stereo, 22050, 8)
+  end
+  
+  def initialize(channels = :mono, sample_rate = 22050, bps = 8)
+    # TODO make sure these values scale to the correct 'shapes'
+    @channels = channels || :mono
+    @sample_rate = sample_rate || 22050
+    @bps = bps || 8
+    @buffer_size = 128
+  end
+  
+  def make_from(file_name)
+    file_name = File.expand_path(file_name)
+    if File.exists?(file_name)
+      wave_header = make_wave_header(file_name)
+      wave_file_name = wave_file_name_from(file_name)
+      write_wave_file(wave_file_name, wave_header, file_name)
+    end
+  end
+  
+  protected
+    def channels_as_data
+      case @channels
+      when :mono
+        1
+      when :stereo
+        2
+      else
+        1
+      end
+    end
+  
+    def number_of_samples_from(file_name)
+      # I dunno, maybe I'll need to pad the file if this is a floaty value?
+      (((File.size(file_name) * 8) / channels_as_data.to_f) / @bps.to_f) if File.exists?(file_name)
+    end
+
+    def make_wave_header(file_name)
+      if File.exists?(file_name)
+        file_size = File.size(file_name)
+        riff = "RIFF"
+        # So .. um .. pack('i') = int.  I want little-endian int and (on my computer at least)
+        # 'l' (system endian long), 'N' (network-endian long) and 'i' give the same
+        # so I'm assuming that 'V' (little-endian long) is ok. (Probably not for other systems)
+        riff << [36 + file_size].pack('V')
+        riff << "WAVE"
+
+        fmt = "fmt "
+
+        number_of_samples = number_of_samples_from(file_name)
+
+        block_align = (channels_as_data * @bps) / 8
+        byte_rate = @sample_rate * block_align
+
+        fmt << [16, 1, channels_as_data, @sample_rate, byte_rate, block_align, @bps].pack('Vv2V2v2')
+
+        data = 'data'
+        # data << [file_size].pack('V')
+        data << [(number_of_samples * channels_as_data * @bps) / 8].pack('V')
+
+        [riff, fmt, data]
+      else
+        nil
+      end
+    end
+
+    def wave_file_name_from(file_name)
+      wave_file_name = "%s.wav" % file_name
+      unless File.exists?(wave_file_name)
+        (1..99).each do |i|
+          wave_file_name = "%s%03d.wav" % [file_name, i] 
+          return wave_file_name unless File.exists?(wave_file_name)
+        end
+        raise "Too many wav files already for this file :(" if i > 999
+      else
+        wave_file_name
+      end
+    end
+
+    def write_wave_file(wave_file_name, header, data_file_name)
+      riff, fmt, data_header = header
+      File.open(wave_file_name, 'w+b') do |wave|
+        wave.write riff
+        wave.write fmt
+        wave.write data_header
+        wave.flush
+
+        File.open(data_file_name, 'rb') do |data|
+          while (d = data.read @buffer_size)
+            wave.write d
+          end
+        end
+        wave.flush
+      end
+    end
+    
+end
