6274d559354ad02dea3f4ac133d817287ad7e52e
Murray Steele <muz@h-lame.com>
2020-01-11 21:12:00 +0000

    Break up the `write_genus_file` method
    It's big and weird and the extension points aren't really that powerful.
    At least breaking it up means we can re-use bits of it if we need to.

--- lib/stegosaurus/genus.rb
+++ lib/stegosaurus/genus.rb
@@ -20,7 +20,33 @@ module Stegosaurus
         genus_file_name
       end
     end
-    
+
+    def with_data_file(data_file_name)
+      File.open(data_file_name, 'rb') do |data_file|
+        yield data_file
+      end
+    end
+
+    def read_from_data_and_write_to_genus(data_file, genus_file)
+      if self.respond_to?(:filter_data, true)
+        write_filtered_data_to_genus(data_file, genus_file)
+      else
+        write_raw_data_to_genus(data_file, genus_file)
+      end
+    end
+
+    def write_filtered_data_to_genus(data_file, genus_file)
+      while (d = data_file.read @buffer_size)
+        genus_file.write filter_data(d)
+      end
+    end
+
+    def write_raw_data_to_genus(data_file, genus_file)
+      while (d = data_file.read @buffer_size)
+        genus_file.write d
+      end
+    end
+
     def write_genus_file(data_file_name, *args)
       genus_file_name = genus_file_name_from(data_file_name)
       File.open(genus_file_name, 'w+b') do |genus_file|
@@ -32,20 +58,12 @@ module Stegosaurus
           end
         end
         genus_file.flush
-        
-        File.open(data_file_name, 'r') do |data_file|
+
+        with_data_file(data_file_name) do |data_file|
           if block_given?
             yield :data, genus_file, data_file, *args
           else
-            if self.respond_to?(:filter_data)
-              while (d = data_file.read @buffer_size)
-                genus_file.write filter_data(d)
-              end
-            else
-              while (d = data_file.read @buffer_size)
-                genus_file.write d
-              end
-            end
+            read_from_data_and_write_to_genus(data_file, genus_file)
           end
         end
         genus_file.flush
